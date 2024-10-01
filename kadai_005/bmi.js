826a5c79bfd41090aff82ae8dd71b3be321a29ac
MJunkoIto <soutozen2018@gmail.com>
2024-10-01 23:05:48 +0900

    Add files via upload

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,1 +1,16 @@
-console.log(68 / (1.7 * 1.7));
+// 変数の宣言
+let weight;
+let height;
+let bmi;
+
+//　体重を代入する
+weight = 68;
+
+//　身長を代入する
+height = 1.7;
+
+//　BMIの計算
+bmi = weight / (height * height);
+
+//　BMIの計算結果の出力
+console.log(bmi);
