59b6c31260ad01539761060fc8a63385a1a9b974
MJunkoIto <soutozen2018@gmail.com>
2024-10-06 12:13:54 +0900

    Add files via upload

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,18 +1,18 @@
 
-  // この数字の意味は不明。３の倍数であり、５の倍数である
+  // 数字は１以上
   let num = 15
 
 //ifを用いてnumberが3と5の倍数のとき「3と5の倍数です」
-if ((number % 3) === 0 && (number % 5) === 0) {
+if ( num % 3 === 0 && num % 5 === 0) {
     console.log('3と5の倍数です')
 }
 
-//if文を用いてnumberが3の倍数のとき「3の倍数です」
-else if ((number % 3) === 0) {
+//if文(else)を用いてnumberが3の倍数のとき「3の倍数です」
+else if (num % 3 === 0) {
     console.log('3の倍数です')
 }
 
 // if文(else)を用いてnumberが5の倍数のとき「5の倍数です」
-else if ((number % 5) ===0) {
+else if (num % 5 ===0) {
     console.log('5の倍数です')
 }
