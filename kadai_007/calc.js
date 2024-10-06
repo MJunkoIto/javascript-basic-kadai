e1a9e5baf9d36f1537d900010e0493700d6cad23
MJunkoIto <soutozen2018@gmail.com>
2024-10-07 00:12:40 +0900

    Add files via upload

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -4,15 +4,18 @@
 
 //ifを用いてnumberが3と5の倍数のとき「3と5の倍数です」
 if ( num % 3 === 0 && num % 5 === 0) {
-    console.log('3と5の倍数です')
+    console.log('3と5の倍数です');
 }
 
 //if文(else)を用いてnumberが3の倍数のとき「3の倍数です」
 else if (num % 3 === 0) {
-    console.log('3の倍数です')
+    console.log('3の倍数です');
 }
 
 // if文(else)を用いてnumberが5の倍数のとき「5の倍数です」
 else if (num % 5 ===0) {
-    console.log('5の倍数です')
+    console.log('5の倍数です');
 }
+
+//　それ以外は変数「num」
+else console.log(num);
