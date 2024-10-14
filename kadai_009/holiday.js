69180e58f0cf162e126a554a55d1ab642a0dd645
MJunkoIto <soutozen2018@gmail.com>
2024-10-15 00:29:28 +0900

    Add files via upload

--- kadai_009/holiday.js
+++ kadai_009/holiday.js
@@ -1,25 +1,16 @@
-//　変数の宣言
-let holidays;
-
 // 値の代入
-holidays = ["正月", "成人の日", "建国記念日", "天皇誕生日", "春分の日", "昭和の日", "憲法記念日", "みどりの日", "こどもの日", "海の日", "山の日", "敬老の日", "秋分の日", "スポーツの日", "文化の日", "勤労感謝の日"];
-
-//変数numにランダムな整数を代入する
-let num = Math.floor(Math.random() * holidays.length);
-
-//変数numの最初の値を出力する(確認用)
-console.log('最初の値は' + holidays.length + 'です');
-
-//変数numの値が配列の範囲内である間、変数numの値を出力しつづける
-while (num !== 0 ) {
-    num = Math.floor(Math.random() * holidays.length);
-
-//次の条件式で比較される、変数numの現在の値を出力する
-console.log('現在の値は' + num + 'です');
-}
+const holidays = ["正月", "成人の日", "建国記念日", "天皇誕生日", "春分の日", "昭和の日", "憲法記念日", "みどりの日", "こどもの日", "海の日", "山の日", "敬老の日", "秋分の日", "スポーツの日", "文化の日", "勤労感謝の日"];
 
 //holidaysの要素を順番に出力する
-for (let i = 0; i <= holidays.length; i++ ) {
-    console.log( holidays.length );
+for (let i = 0; i < 16; i++ ) {
+    console.log( holidays[i]);
 }
+let i = 0;
+while (i < 16 ) { 
+    console.log( holidays[i]);
+    i++;    
+}
+
+
+
 
